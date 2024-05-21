00785b09f286cadd2a5c8337cbb428eb4fd5413d
chocsteryu <99021003+choccymilku@users.noreply.github.com>
2024-05-21 21:39:10 +0200

    breh

--- commands/slash commands/IMAGE/captionTop.js
+++ commands/slash commands/IMAGE/captionTop.js
@@ -6,8 +6,8 @@ const ffmpegPath = require('ffmpeg-static');
 const { ButtonBuilder, ActionRowBuilder, ButtonStyle } = require('discord.js');
 const ffprobePath = require('ffprobe-static').path;
 const sharp = require('sharp');
-
 const path = require('path');
+const TextToSVG = require('text-to-svg');
 
 dotenv.config();
 
@@ -17,14 +17,13 @@ ffmpeg.setFfprobePath(ffprobePath);
 module.exports = async function handleInteraction(interaction) {
     if (interaction.isCommand() && interaction.commandName === 'caption-top') {
         const caption = interaction.options.getString('caption');
-        console.log('top caption')
+        console.log('Top caption:', caption);
 
         const randomName = Math.floor(Math.random() * 100000000);
         const originalImagePath = `temp/${randomName}-CAPTION.jpg`;
         const overlaidImagePath = `temp/${randomName}-CAPTION-OVERLAID.jpg`;
         const overlayText = caption;
 
-        // Constants for configuration
         const MAX_FONT_SIZE = 60;
         const MIN_FONT_SIZE = 20;
 
@@ -36,10 +35,7 @@ module.exports = async function handleInteraction(interaction) {
 
             if (attachmentType === 'video') {
                 await interaction.followUp({
-                    embeds: [{
-                        title: 'This command is not available for videos',
-                        color: 0xff0000
-                    }],
+                    embeds: [{ title: 'This command is not available for videos', color: 0xff0000 }],
                     ephemeral: true
                 });
                 return;
@@ -59,12 +55,9 @@ module.exports = async function handleInteraction(interaction) {
 
             const { width, height } = originalImageMetadata.streams[0];
 
-            // Adjust the font size based on the image width
             const FONT_SIZE = Math.min(Math.max(width / 10, MIN_FONT_SIZE), MAX_FONT_SIZE);
-
             console.log('Font size:', FONT_SIZE);
 
-            // Dynamically determine the maximum number of characters per line
             const maxCharsPerLine = Math.floor(width / (FONT_SIZE / 2));
             const lines = [];
             let line = '';
@@ -80,26 +73,19 @@ module.exports = async function handleInteraction(interaction) {
             console.log('Max characters per line:', maxCharsPerLine);
             console.log('Lines:', lines);
 
-            const TextToSVG = require('text-to-svg');
-
             const textToSVG = TextToSVG.loadSync("fonts/Futura.ttf");
 
-            // Create a new image for each line
             for (let i = 0; i < lines.length; i++) {
                 const line = lines[i];
 
-                // Create the overlay SVG for the current line
                 const lineNew = textToSVG.getSVG(line, {
                     fontSize: FONT_SIZE,
                     anchor: "centre top",
-                    attributes: {
-                        fill: "black",
-                    },
+                    attributes: { fill: "black" },
                 });
 
-                void sharp(Buffer.from(lineNew)).toFile(`temp/line${i}.png`);   
-                
-                // get the width and height of the image using ffprobe
+                await sharp(Buffer.from(lineNew)).toFile(`temp/line${i}.png`);
+
                 const imageMetadata = await new Promise((resolve, reject) => {
                     ffmpeg.ffprobe(`temp/line${i}.png`, (err, metadata) => {
                         if (err) reject(err);
@@ -111,14 +97,12 @@ module.exports = async function handleInteraction(interaction) {
                 console.log('imageMetadata width:', imageMetadata.streams[0].width);
                 console.log('imageMetadata height:', imageMetadata.streams[0].height);
 
-                // calculate the left offset for each text image
                 const leftOffset = Math.round((width - imageMetadata.streams[0].width) / 2);
                 console.log('left offset:', leftOffset);
 
-                // create a white box with the height of the text image and width of the original image
                 const boxHeight = imageMetadata.streams[0].height;
                 const boxWidth = width;
-                const totalBoxHeight =  Math.round(boxHeight * (i + 1) + (boxHeight / 2));
+                const totalBoxHeight = Math.round(boxHeight * (i + 1) + (boxHeight / 2));
                 console.log('total box height:', totalBoxHeight);
 
                 await sharp({
@@ -128,48 +112,37 @@ module.exports = async function handleInteraction(interaction) {
                         channels: 4,
                         background: { r: 255, g: 255, b: 255, alpha: 0 }
                     }
-                })
-                .toFile(`temp/box${i}-white.png`);
+                }).toFile(`temp/box${i}-white.png`);
 
-                // composite the text image on top of the white box
                 await sharp(`temp/box${i}-white.png`)
-                .composite([{
-                    input: `temp/line${i}.png`,
-                    top: 0,
-                    left: leftOffset
-                }])
-                .toFile(`temp/line${i}-composite.png`);
+                    .composite([{ input: `temp/line${i}.png`, top: 0, left: leftOffset }])
+                    .toFile(`temp/line${i}-composite.png`);
                 sharp.cache(false);
                 fs.unlinkSync(`temp/line${i}.png`);
                 fs.unlinkSync(`temp/box${i}-white.png`);
 
-                // get all of the images and composite them together by stacking them vertically
                 const images = [];
                 for (let j = 0; j <= i; j++) {
                     images.push(`temp/line${j}-composite.png`);
                 }
 
-                // get the original image, extend it by the height of the box, and composite the text images on top
                 await sharp(originalImagePath)
-                .extend({
-                    top: totalBoxHeight,
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                    background: { r: 255, g: 255, b: 255, alpha: 1 }
-                })
-                .composite(images.map((image) => ({
-                    input: image,
-                    top: images.indexOf(image) * boxHeight + Math.round((boxHeight / 4)),
-                    left: 0,
-                }))
-                )
-                .toFile(overlaidImagePath);
+                    .extend({
+                        top: totalBoxHeight,
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                        background: { r: 255, g: 255, b: 255, alpha: 1 }
+                    })
+                    .composite(images.map((image, idx) => ({
+                        input: image,
+                        top: idx * boxHeight + Math.round((boxHeight / 4)),
+                        left: 0,
+                    })))
+                    .toFile(overlaidImagePath);
             }
-        
-            await interaction.followUp({
-                files: [overlaidImagePath]
-            });
+
+            await interaction.followUp({ files: [overlaidImagePath] });
             fs.unlinkSync(originalImagePath);
             fs.unlinkSync(`temp/line0-composite.png`);
 
